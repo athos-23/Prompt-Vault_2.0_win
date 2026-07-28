5030f4c5e78d955c5610339d6f6d6efde25b01cc
athos.23 <hashassin33@protonmail.com>
2026-07-28 06:50:48 +0200

    Set DataContext to MainViewModel in MainWindow

--- PromptVault.App/MainWindow.xaml.cs
+++ PromptVault.App/MainWindow.xaml.cs
@@ -1,4 +1,16 @@
+using PromptVault.App.ViewModels;
+
 namespace PromptVault.App;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        DataContext = new MainViewModel();
+    }
+}namespace PromptVault.App;
 
 public partial class MainWindow : Window
 {
