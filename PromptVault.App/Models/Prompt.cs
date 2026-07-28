16158122c5d927ccc37bf3777b028aa190c7961d
athos.23 <hashassin33@protonmail.com>
2026-07-28 07:02:45 +0200

    Implement INotifyPropertyChanged in Prompt model

--- PromptVault.App/Models/Prompt.cs
+++ PromptVault.App/Models/Prompt.cs
@@ -1,20 +1,59 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace PromptVault.App.Models;
 
-public class Prompt
+public class Prompt : INotifyPropertyChanged
 {
     public int Id { get; set; }
 
-    public string Title { get; set; } = "";
+    private string title = "";
+    public string Title
+    {
+        get => title;
+        set { title = value; OnPropertyChanged(); }
+    }
 
-    public string Project { get; set; } = "";
 
-    public string Tags { get; set; } = "";
+    private string project = "";
+    public string Project
+    {
+        get => project;
+        set { project = value; OnPropertyChanged(); }
+    }
 
-    public string Content { get; set; } = "";
+
+    private string tags = "";
+    public string Tags
+    {
+        get => tags;
+        set { tags = value; OnPropertyChanged(); }
+    }
+
+
+    private string content = "";
+    public string Content
+    {
+        get => content;
+        set { content = value; OnPropertyChanged(); }
+    }
+
 
     public string Color { get; set; } = "default";
 
     public DateTime CreatedAt { get; set; }
 
     public DateTime UpdatedAt { get; set; }
+
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+
+    private void OnPropertyChanged(
+        [CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(
+            this,
+            new PropertyChangedEventArgs(propertyName));
+    }
 }
